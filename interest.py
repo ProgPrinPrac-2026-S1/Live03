67e950c6e718b5fdf914ff25ab278e47e31f2d24
N. A. Ronald <naronald@users.noreply.github.com>
2026-03-17 18:16:06 +1100

    increased time periods

--- interest.py
+++ interest.py
@@ -2,7 +2,7 @@ def interest(principal, rate):
     amount = principal * (1 + rate)
     return amount
 
-for rate in range(1, 5):
+for rate in range(1, 10):
     principal = 1000
     amount = interest(principal, rate/100.0)
     print(f"Principal: ${principal:.2f}  Rate: {rate}%  Amount: ${amount:.2f}")
