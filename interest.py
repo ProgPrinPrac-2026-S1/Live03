6b39eff4915f4d2144c13d272f3bc634018a31f8
Nicole Ronald <nronald@swin.edu.au>
2026-03-17 18:15:29 +1100

    added iteration

--- interest.py
+++ interest.py
@@ -1,3 +1,8 @@
 def interest(principal, rate):
     amount = principal * (1 + rate)
     return amount
+
+for rate in range(1, 5):
+    principal = 1000
+    amount = interest(principal, rate/100.0)
+    print(f"Principal: ${principal:.2f}  Rate: {rate}%  Amount: ${amount:.2f}")
